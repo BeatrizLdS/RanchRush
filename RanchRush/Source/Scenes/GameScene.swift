0a9e2c3cfd34cdcabcaabddb2929959ae2e5a284
BeatrizLdS <beatriz_.silva@hotmail.com>
2023-04-05 09:28:28 -0300

    :sparkles: feat: Creating random obstacles and moving this obstacles in GameScene

--- RanchRush/Source/Scenes/GameScene.swift
+++ RanchRush/Source/Scenes/GameScene.swift
@@ -10,13 +10,66 @@ import GameplayKit
 
 class GameScene: SKScene {
     
+    let obstacleTypes: [ObstacleType] = [.female, .male]
+    var numberObstacles = 5
+    var obstaclesSpeed: CGFloat = 0.1
+    
     override func didMove(to view: SKView) {
-        backgroundColor = .systemPink
+        backgroundColor = .black
     }
     
     override func sceneDidLoad() {
     }
     
     override func update(_ currentTime: TimeInterval) {
+        let activeObstacles = children.compactMap{ $0 as? Obstacle}
+        
+        for child in activeObstacles {
+            if child.frame.maxX < 0 {
+                if !frame.intersects(child.frame) {
+                    child.removeFromParent()
+                }
+            }
+        }
+        
+        if activeObstacles.isEmpty {
+            createObstacles()
+            numberObstacles += 2
+            if obstaclesSpeed < 1 {
+                obstaclesSpeed += 0.05
+            }
+        }
+    }
+    
+    func createObstacles() {
+        let obstacleOffsetx: CGFloat = 0.4
+        let obstacleStartX: Double = 0.5
+        
+        generateRandomObstacles(
+            startX: obstacleStartX,
+            spaceBetween: obstacleOffsetx,
+            numberObstacles: self.numberObstacles
+        )
+    }
+    
+    func generateRandomObstacles(startX: Double, spaceBetween: CGFloat, numberObstacles: Int) {
+        var obstacletypeIndex = Int.random(in: 0..<obstacleTypes.count)
+        let positions = Array(stride(from: 0.05, to: 0.5, by: 0.05)) // posições dos inimigos
+
+        for index in 0...numberObstacles {
+            obstacletypeIndex = Int.random(in: 0..<obstacleTypes.count)
+            let space = spaceBetween * CGFloat(index) + (positions.randomElement() ?? 0)
+            
+            let startPosition = CGPoint(x: frame.maxX, y: frame.minY + 0.1)
+            let obstacle = Obstacle(obstacleType: obstacleTypes[obstacletypeIndex],
+                                    startPosition: startPosition,
+                                    xOffset: space,
+                                    speed: obstaclesSpeed)
+            obstacle.xScale = 0.1
+            obstacle.yScale = 0.3
+            obstacle.calculateSize(windowWidth: frame.width, windowHeight: frame.height)
+            obstacle.anchorPoint = .zero
+            addChild(obstacle)
+        }
     }
 }
---
+++ RanchRush/Source/SpriteNodes/Obstacle.swift
@@ -0,0 +1,72 @@
+//
+//  Obstacle.swift
+//  RanchRush
+//
+//  Created by Beatriz Leonel da Silva on 04/04/23.
+//
+
+import SpriteKit
+
+enum ObstacleState: String {
+    case idle = "Idle"
+    case attack = "Attack"
+}
+
+enum ObstacleType: String {
+    case female = "Female"
+    case male = "Male"
+}
+
+class Obstacle: SKSpriteNode {
+    private var obstacleType: ObstacleType
+    private lazy var frames: [SKTexture] = []
+    private var state: ObstacleState = .idle
+    private var velocity: CGFloat
+
+    init(obstacleType: ObstacleType, startPosition: CGPoint, xOffset: CGFloat, speed: CGFloat) {
+        self.obstacleType = obstacleType
+        self.velocity = speed
+        super.init(texture: SKTexture(imageNamed: "zombie"), color: .white, size: .zero)
+//        setPhysics()
+        setObstacle(startPosition: startPosition, xOffset: xOffset)
+        configureMovement()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setFrames () {
+        let newFrames = getFrames(with: state.rawValue, atlasName: "\(obstacleType.rawValue)Zombie")
+        if newFrames.isEmpty == false {
+            frames = newFrames
+            texture = frames[0]
+        }
+    }
+    
+    func configureMovement() {
+        let path = UIBezierPath()
+        path.move(to: .zero)
+        path.addLine(to: CGPoint(x: -100, y: 0))
+        
+        let movement = SKAction.follow(
+            path.cgPath,
+            asOffset: true,
+            orientToPath: false,
+            speed: velocity
+        )
+        run(movement)
+    }
+    
+}
+
+extension Obstacle: SetObstacleProtocol {
+    func setPosition(startPosition: CGPoint, xOffset: CGFloat) {
+        position = CGPoint(x: startPosition.x + xOffset, y: startPosition.y)
+    }
+    
+    func setPhysics() {
+        setFrames()
+    }
+    
+}
