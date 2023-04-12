b089bde533f6ed392c4f7eaf328237432c6381ca
BeatrizLdS <beatriz_.silva@hotmail.com>
2023-04-12 08:37:13 -0300

    :construction:

---
+++ RanchRush/Source/Model/ScenarioObjects.swift
@@ -0,0 +1,20 @@
+//
+//  ScenarioObjects.swift
+//  RanchRush
+//
+//  Created by Beatriz Leonel da Silva on 12/04/23.
+//
+
+import Foundation
+
+enum ScenarioObjectsType: String {
+    case fance = "fance"
+    case tractor = "tractor"
+    case tree = "tree"
+    case barn = "barn"
+}
+
+enum ScenarioType: String {
+    case random = "random"
+    case starting = "starting"
+}
---
+++ RanchRush/Source/SpriteNodes/Scenario.swift
@@ -0,0 +1,23 @@
+//
+//  Scenario.swift
+//  RanchRush
+//
+//  Created by Beatriz Leonel da Silva on 12/04/23.
+//
+
+import SpriteKit
+
+class Scenario: SKSpriteNode {
+    private var objectType: ScenarioObjectsType
+    var scenarioType: ScenarioType
+    
+    init(objectType: ScenarioObjectsType, scenarioType: ScenarioType) {
+        self.objectType = objectType
+        self.scenarioType = scenarioType
+        super.init(texture: SKTexture(imageNamed: objectType.rawValue), color: .clear, size: .zero)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
